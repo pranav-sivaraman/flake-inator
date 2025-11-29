ea2c0244451614b60da0bd2be8efd696f409e712
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-11-29 11:25:42 -0500

    psivaram: add dev packages
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/psivaram.nix
+++ modules/psivaram.nix
@@ -2,7 +2,7 @@ let
   userName = "psivaram";
 in
 {
-  flake.modules.nixos.${userName} = {
+  flake.modules.nixos.${userName} = { pkgs, ... }: {
     users.users.${userName} = {
       isNormalUser = true;
       description = "psivaram";
@@ -11,6 +11,24 @@ in
         "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBm/NvZHRsOINhjseCZ7aI2DbpaNPyZjw+eXPXpSRvlqAAAAEnNzaDphdXRoZW50aWNhdGlvbg=="
         "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHiGaA36EZ/k/prPZpZwDN2e85UCTkmlCSmk1StomRqhAAAAEnNzaDphdXRoZW50aWNhdGlvbg=="
       ];
+      packages = with pkgs; [
+        jujutsu
+      ];
+    };
+
+    programs = {
+      neovim = {
+        enable = true;
+        viAlias = true;
+        vimAlias = true;
+      };
+      git.enable = true;
+      bat.enable = true;
+      zoxide = {
+        enable = true;
+        enableFishIntegration = false;
+      };
+      fish.enable = true;
     };
   };
 }
