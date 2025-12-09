3e7a038e67cf82ef1f72e83ea236e656f7fb8a3b
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-12-08 20:35:43 -0500

    Revert "initrd: copy hostkey-initrd file"
    This reverts commit 7b748e3fc2f35658d1a31400a8e972db211e45c8.

--- modules/hosts/vm/boot.nix
+++ modules/hosts/vm/boot.nix
@@ -7,8 +7,6 @@
       hostkey-initrd = {
         rekeyFile = self + "/modules/secrets/${config.networking.hostName}/hostkey-initrd.age";
         generator.script = "ssh-ed25519";
-        path = "/etc/initrd-hostkey";
-        symlink = false;
       };
     };
   };
