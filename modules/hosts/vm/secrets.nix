2a1d102c471015cbf14092570a87d0aa3b39cea8
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-12-08 20:35:43 -0500

    Revert "zfs: enable remote ssh unlock"
    This reverts commit ea7b977edf2a3eaa77711ead81865471ebedda1d.

--- modules/hosts/vm/secrets.nix
+++ modules/hosts/vm/secrets.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, self, ... }:
 {
   flake.modules.nixos.vm =
     { config, ... }:
@@ -6,6 +6,12 @@
       age = {
         rekey = {
           hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuPTKBNA2RQBlZeiN1E7fUdg0V++U1Ln5HQlYg5NI6X root@vm";
+        };
+        secrets = {
+          randomPassword = {
+            rekeyFile = self + /modules/secrets/randomPassword.age;
+            generator.script = "passphrase";
+          };
         };
       };
     };
