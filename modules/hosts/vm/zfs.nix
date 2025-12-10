6c9cadcd166c2b48b5d5d9b0b948abeb7f673279
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-12-10 12:32:29 -0500

    impermanence: setup scripts and systemd services
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/hosts/vm/zfs.nix
+++ modules/hosts/vm/zfs.nix
@@ -1,84 +1,108 @@
 { inputs, ... }:
 {
-  flake.modules.nixos.vm = {
-    imports = [ inputs.disko.nixosModules.disko ];
-    disko.devices = {
-      disk = {
-        root = {
-          type = "disk";
-          device = "/dev/vda";
-          content = {
-            type = "gpt";
-            partitions = {
-              ESP = {
-                size = "1G";
-                type = "EF00";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot";
-                  mountOptions = [ "nofail" ];
+  flake.modules.nixos.vm =
+    { pkgs, ... }:
+    {
+      imports = [ inputs.disko.nixosModules.disko ];
+      disko.devices = {
+        disk = {
+          root = {
+            type = "disk";
+            device = "/dev/vda";
+            content = {
+              type = "gpt";
+              partitions = {
+                ESP = {
+                  size = "1G";
+                  type = "EF00";
+                  content = {
+                    type = "filesystem";
+                    format = "vfat";
+                    mountpoint = "/boot";
+                    mountOptions = [ "nofail" ];
+                  };
+                };
+                zfs = {
+                  size = "100%";
+                  content = {
+                    type = "zfs";
+                    pool = "rpool";
+                  };
                 };
               };
-              zfs = {
-                size = "100%";
-                content = {
-                  type = "zfs";
-                  pool = "rpool";
+            };
+          };
+        };
+        zpool = {
+          rpool = {
+            type = "zpool";
+            options = {
+              ashift = "12";
+              autotrim = "on";
+            };
+            rootFsOptions = {
+              acltype = "posixacl";
+              compression = "zstd";
+              mountpoint = "none";
+              xattr = "sa";
+            };
+
+            datasets = {
+              local = {
+                type = "zfs_fs";
+                options.mountpoint = "none";
+                postCreateHook = "zfs snapshot rpool/local/root@blank";
+              };
+              safe = {
+                type = "zfs_fs";
+                options = {
+                  mountpoint = "none";
+                  "com.sun:auto-snapshot" = "true";
                 };
+              };
+
+              "local/root" = {
+                type = "zfs_fs";
+                mountpoint = "/";
+              };
+              "local/nix" = {
+                type = "zfs_fs";
+                mountpoint = "/nix";
+              };
+              "safe/persist" = {
+                type = "zfs_fs";
+                mountpoint = "/persist";
+              };
+              "safe/home" = {
+                type = "zfs_fs";
+                mountpoint = "/home";
               };
             };
           };
         };
       };
-      zpool = {
-        rpool = {
-          type = "zpool";
-          options = {
-            ashift = "12";
-            autotrim = "on";
-          };
-          rootFsOptions = {
-            acltype = "posixacl";
-            compression = "zstd";
-            mountpoint = "none";
-            xattr = "sa";
-          };
 
-          datasets = {
-            local = {
-              type = "zfs_fs";
-              options.mountpoint = "none";
-            };
-            safe = {
-              type = "zfs_fs";
-              options = {
-                mountpoint = "none";
-                "com.sun:auto-snapshot" = "true";
-              };
-            };
+      fileSystems."/persist".neededForBoot = true;
 
-            "local/root" = {
-              type = "zfs_fs";
-              mountpoint = "/";
-            };
-            "local/nix" = {
-              type = "zfs_fs";
-              mountpoint = "/nix";
-            };
-            "safe/persist" = {
-              type = "zfs_fs";
-              mountpoint = "/persist";
-            };
-            "safe/home" = {
-              type = "zfs_fs";
-              mountpoint = "/home";
-            };
-          };
-        };
+      boot.initrd.systemd.services.rollback = {
+        description = "Rollback ZFS datasets to a pristine state";
+        wantedBy = [
+          "initrd.target"
+        ];
+        after = [
+          "zfs-import-rpool.service"
+        ];
+        before = [
+          "sysroot.mount"
+        ];
+        path = with pkgs; [
+          zfs
+        ];
+        unitConfig.DefaultDependencies = "no";
+        serviceConfig.Type = "oneshot";
+        script = ''
+          zfs rollback -r rpool/local/root@blank && echo "rollback complete"
+        '';
       };
     };
-
-    fileSystems."/persist".neededForBoot = true;
-  };
 }
