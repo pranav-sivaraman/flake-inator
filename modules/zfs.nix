33385e0dd580dc826d0800f60b61dcb54a069b51
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-12-10 16:18:11 -0500

    zfs: disable zfs auto snapshot
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -5,15 +5,6 @@
     services.zfs = {
       trim.enable = true;
       autoScrub.enable = true;
-
-      autoSnapshot = {
-        enable = true;
-        frequent = 4;
-        hourly = 24;
-        daily = 7;
-        weekly = 4;
-        monthly = 12;
-      };
     };
   };
 }
