7ee5a806abaf7931fc4cae5ec0df8333560955ef
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-12-10 15:25:11 -0500

    impermanence: mount fixes and also home-manager fixes
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/hosts/vm/zfs.nix
+++ modules/hosts/vm/zfs.nix
@@ -62,29 +62,29 @@
 
               "local/root" = {
                 type = "zfs_fs";
-                options.mountpoint = "/";
                 mountpoint = "/";
                 postCreateHook = "zfs list -t snapshot rpool/local/root@blank > /dev/null 2>&1 || zfs snapshot rpool/local/root@blank";
               };
               "local/nix" = {
                 type = "zfs_fs";
-                options.mountpoint = "/nix";
                 mountpoint = "/nix";
               };
               "safe/persist" = {
                 type = "zfs_fs";
-                options.mountpoint = "/persist";
                 mountpoint = "/persist";
               };
               "safe/home" = {
                 type = "zfs_fs";
-                options.mountpoint = "/home";
                 mountpoint = "/home";
               };
             };
           };
         };
       };
+
+      fileSystems."/persist".neededForBoot = true;
+
+      systemd.services.zfs-mount.enable = false;
 
       boot.initrd.systemd.services.rollback = {
         description = "Rollback ZFS datasets to a pristine state";
