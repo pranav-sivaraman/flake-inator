91de97cd90b30f449ba3e793ebfc14e39cf1d79c
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-12-11 08:01:45 -0500

    impermanence: bugfix for zfs rollback
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/impermenance/rollback.nix
+++ modules/impermenance/rollback.nix
@@ -1,26 +1,27 @@
-{ pkgs, ... }:
 {
-  flake.modules.nixos.impermanence = {
-    # TODO: may need to setup some guard/type checking for rpool?
-    boot.initrd.systemd.services.rollback = {
-      description = "Rollback ZFS datasets to a pristine state";
-      wantedBy = [
-        "initrd.target"
-      ];
-      after = [
-        "zfs-import-rpool.service"
-      ];
-      before = [
-        "sysroot.mount"
-      ];
-      path = with pkgs; [
-        zfs
-      ];
-      unitConfig.DefaultDependencies = "no";
-      serviceConfig.Type = "oneshot";
-      script = ''
-        zfs rollback -r rpool/local/root@blank && echo "rollback complete"
-      '';
+  flake.modules.nixos.impermanence =
+    { pkgs, ... }:
+    {
+      # TODO: may need to setup some guard/type checking for rpool?
+      boot.initrd.systemd.services.rollback = {
+        description = "Rollback ZFS datasets to a pristine state";
+        wantedBy = [
+          "initrd.target"
+        ];
+        after = [
+          "zfs-import-rpool.service"
+        ];
+        before = [
+          "sysroot.mount"
+        ];
+        path = with pkgs; [
+          zfs
+        ];
+        unitConfig.DefaultDependencies = "no";
+        serviceConfig.Type = "oneshot";
+        script = ''
+          zfs rollback -r rpool/local/root@blank && echo "rollback complete"
+        '';
+      };
     };
-  };
 }
