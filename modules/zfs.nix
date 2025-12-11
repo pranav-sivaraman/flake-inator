d1ba5a21a773218e6455b4302c22717daca3912e
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-12-11 12:47:33 -0500

    zfs: fix sanoid config
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -9,18 +9,18 @@
       };
       sanoid = {
         enable = true;
-        interval = "*:0/10";  # Run every 10 minutes
+        interval = "*:0/10"; # Run every 10 minutes
         templates = {
           default = {
             autosnap = true;
             autoprune = true;
-            frequently_period = 10;
-            frequent = 12;   # Every 10 min, keep for 2 hours
-            hourly = 48;     # Every hour, keep for 2 days
-            daily = 14;      # Every day, keep for 2 weeks
-            weekly = 8;      # Every Sunday, keep for ~2 months
-            monthly = 24;    # Every month, keep for 2 years
-            yearly = 5;      # Every year, keep for 5 years
+            frequent_period = 10;
+            frequently = 12; # Every 10 min, keep for 2 hours
+            hourly = 48; # Every hour, keep for 2 days
+            daily = 14; # Every day, keep for 2 weeks
+            weekly = 8; # Every Sunday, keep for ~2 months
+            monthly = 24; # Every month, keep for 2 years
+            yearly = 5; # Every year, keep for 5 years
           };
         };
         datasets = {
