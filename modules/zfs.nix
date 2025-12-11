49cef966e2d52d559b2651ca8835c9cb05b42ca7
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-12-11 13:18:04 -0500

    ZFS Snapshotting with Sanoid (#2)
    * zfs: disable zfs auto snapshot
    
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>
    
    * zfs: use sanoid for snapshot instead
    
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>
    
    * zfs: fix interval for sanoid
    
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>
    
    * zfs: fix sanoid config
    
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>
    
    ---------
    
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -2,17 +2,30 @@
   flake.modules.nixos.zfs = {
     systemd.services.zfs-mount.enable = false;
 
-    services.zfs = {
-      trim.enable = true;
-      autoScrub.enable = true;
-
-      autoSnapshot = {
+    services = {
+      zfs = {
+        trim.enable = true;
+        autoScrub.enable = true;
+      };
+      sanoid = {
         enable = true;
-        frequent = 4;
-        hourly = 24;
-        daily = 7;
-        weekly = 4;
-        monthly = 12;
+        interval = "*:0/10"; # Run every 10 minutes
+        templates = {
+          default = {
+            autosnap = true;
+            autoprune = true;
+            frequent_period = 10;
+            frequently = 12; # Every 10 min, keep for 2 hours
+            hourly = 48; # Every hour, keep for 2 days
+            daily = 14; # Every day, keep for 2 weeks
+            weekly = 8; # Every Sunday, keep for ~2 months
+            monthly = 24; # Every month, keep for 2 years
+            yearly = 5; # Every year, keep for 5 years
+          };
+        };
+        datasets = {
+          "rpool/safe".useTemplate = [ "default" ];
+        };
       };
     };
   };
