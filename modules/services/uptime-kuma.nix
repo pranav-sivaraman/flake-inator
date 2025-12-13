63bf56911b23147becae603c343de24116373667
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-12-13 11:50:48 -0500

    services: expose uptime-kuma to local network
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/services/uptime-kuma.nix
+++ modules/services/uptime-kuma.nix
@@ -2,6 +2,9 @@
   flake.modules.nixos.uptime-kuma = {
     services.uptime-kuma = {
       enable = true;
+      settings = {
+        HOST = "192.168.64.2"; # Listen on all interfaces, or use your specific static IP
+      };
     };
 
     environment.persistence."/persist".directories = [
