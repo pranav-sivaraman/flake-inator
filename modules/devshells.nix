0455def8af8684a5baaa791d77cb53a565dec36b
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-12-30 17:37:00 -0500

    vm: generate vars secrets
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/devshells.nix
+++ modules/devshells.nix
@@ -9,6 +9,9 @@ _: {
       devShells = {
         default = pkgs.mkShellNoCC {
           buildInputs = [ pkgs.fish ];
+          env = {
+            CLAN_NO_COMMIT = 1;
+          };
           shellHook = ''
             # Launch fish if not already in fish
             if [ -z "$FISH_VERSION" ]; then
