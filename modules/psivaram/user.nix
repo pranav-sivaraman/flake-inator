68b5440a2e16d35008ea4f5a996713daa2e70636
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-12-31 14:44:37 -0500

    Add agentc node (#7)
    * agentc: new service node
    
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>
    
    * psivaram: add clan specific info
    
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>
    
    * agentc: generate vars
    
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>
    
    * agentc: add facter.json
    
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>
    
    * agentc: apply disko
    
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>
    
    ---------
    
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/psivaram/user.nix
+++ modules/psivaram/user.nix
@@ -1,13 +1,29 @@
-{ ... }:
+{ inputs, ... }:
 let
   userName = import ./_config.nix;
 in
 {
+  clan.inventory.instances = {
+    psivaram-user = {
+      module.name = "users";
+      roles.default.tags.all = { }; # Add this user to all machines
+      roles.default.settings = {
+        user = "psivaram";
+        groups = [
+          "wheel" # Allow using 'sudo'
+          "networkmanager" # Allows to manage network connections.
+          "video" # Allows to access video devices.
+          "input" # Allows to access input devices.
+        ];
+        prompt = false;
+        share = true;
+      };
+    };
+  };
   flake.modules.nixos.${userName} = {
     users.users.${userName} = {
       isNormalUser = true;
       description = "psivaram";
-      extraGroups = [ "wheel" ];
       openssh.authorizedKeys.keys = [
         "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBm/NvZHRsOINhjseCZ7aI2DbpaNPyZjw+eXPXpSRvlqAAAAEnNzaDphdXRoZW50aWNhdGlvbg=="
         "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHiGaA36EZ/k/prPZpZwDN2e85UCTkmlCSmk1StomRqhAAAAEnNzaDphdXRoZW50aWNhdGlvbg=="
