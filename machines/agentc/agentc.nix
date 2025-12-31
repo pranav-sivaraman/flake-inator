f1f7245bd62b461eaf5284d1d60254c9b8d23e1b
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-12-30 21:59:44 -0500

    agentc: new service node
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

---
+++ machines/agentc/agentc.nix
@@ -0,0 +1,21 @@
+{ inputs, ... }:
+{
+  clan.machines.agentc = {
+    nixpkgs.hostPlatform = "x86_64-linux";
+    imports = with inputs.self.modules.nixos; [
+      boot
+      impermanence
+      locale
+      firewall
+      networkd
+      nix
+      psivaram
+      remote-unlock
+      security
+      ssh
+      users
+      zfs
+    ];
+  };
+
+}
