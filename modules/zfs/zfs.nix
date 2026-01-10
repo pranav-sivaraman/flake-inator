e481f49ca4881078dfa5e8f1d300c32656a3a2f0
Pranav Sivaraman <pranavsivaraman@gmail.com>
2026-01-10 16:10:44 -0500

    modules: some fixes for vms and utils
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/zfs/zfs.nix
+++ modules/zfs/zfs.nix
@@ -2,6 +2,8 @@
   flake.modules.nixos.zfs = {
     systemd.services.zfs-mount.enable = false;
 
+    boot.zfs.devNodes = "/dev/disk/by-partuuid";
+
     services.zfs = {
       trim.enable = true;
       autoScrub.enable = true;
