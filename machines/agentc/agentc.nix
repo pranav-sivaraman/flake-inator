821981bad18e627f8afd2e10a48da8a66b3f5a2c
Pranav Sivaraman <pranavsivaraman@gmail.com>
2026-01-19 18:09:31 -0500

    agentc: set target host
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- machines/agentc/agentc.nix
+++ machines/agentc/agentc.nix
@@ -1,5 +1,10 @@
 { inputs, ... }:
 {
+  clan.inventory.machines = {
+    agentc = {
+      deploy.targetHost = "psivaram@192.168.1.3"; # TODO: define this somewhere to easily reference?
+    };
+  };
   clan.machines.agentc = {
     nixpkgs.hostPlatform = "x86_64-linux";
     imports = with inputs.self.modules.nixos; [
