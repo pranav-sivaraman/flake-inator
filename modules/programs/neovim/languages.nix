361b21b4d3c112d575375ca9850eea14a6e6281f
Pranav Sivaraman <pranavsivaraman@gmail.com>
2026-02-26 14:19:14 -0500

    neovim: fix neocmakelsp activation
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/programs/neovim/languages.nix
+++ modules/programs/neovim/languages.nix
@@ -1,39 +1,46 @@
 {
   flake.aspects.shell = {
-    homeManager = {
-      programs.nvf.settings.vim = {
-        languages = {
-          enableTreesitter = true;
-          enableDAP = true;
-          enableFormat = true;
-          enableExtraDiagnostics = true;
+    homeManager =
+      { lib, pkgs, ... }:
+      {
+        programs.nvf.settings.vim = {
+          lsp.servers.neocmakelsp.cmd = lib.mkForce [
+            "${lib.getExe pkgs.neocmakelsp}"
+            "stdio"
+          ];
 
-          python = {
-            enable = true;
-            format.type = [ "ruff" ];
-            lsp.servers = [ "ty" ];
+          languages = {
+            enableTreesitter = true;
+            enableDAP = true;
+            enableFormat = true;
+            enableExtraDiagnostics = true;
+
+            python = {
+              enable = true;
+              format.type = [ "ruff" ];
+              lsp.servers = [ "ty" ];
+            };
+
+            nix = {
+              enable = true;
+              format.type = [ "nixfmt" ];
+              lsp.servers = [ "nixd" ];
+            };
+
+            assembly.enable = true;
+            bash.enable = true;
+            hcl.enable = true;
+            make.enable = true;
+            cmake.enable = true;
+            clang.enable = true;
+            rust.enable = true;
+            markdown.enable = true;
+            typst.enable = true;
+            toml.enable = true;
+            lua.enable = true;
+            yaml.enable = true;
           };
-
-          nix = {
-            enable = true;
-            format.type = [ "nixfmt" ];
-            lsp.servers = [ "nixd" ];
-          };
-
-          assembly.enable = true;
-          bash.enable = true;
-          hcl.enable = true;
-          make.enable = true;
-          cmake.enable = true;
-          clang.enable = true;
-          rust.enable = true;
-          markdown.enable = true;
-          typst.enable = true;
-          toml.enable = true;
-          lua.enable = true;
-          yaml.enable = true;
         };
       };
-    };
   };
 }
