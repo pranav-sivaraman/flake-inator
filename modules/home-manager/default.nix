a2a19825fb9519a84025c3365ee9e2db56a26226
Pranav Sivaraman <pranavsivaraman@gmail.com>
2026-02-25 19:23:38 -0500

    home-manager: adjust base modules
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -4,28 +4,35 @@ let
     psivaram.homeManager
     nix.homeManager
     shell.homeManager
-    ssh.homeManager
   ];
 in
 {
   flake.homeManagerModules = {
     base = baseModules;
-    full = baseModules ++ (with inputs.self.aspects; [
-      window-manager.homeManager
-      desktop.homeManager
-      mac.homeManager
-    ]);
+    full =
+      baseModules
+      ++ (with inputs.self.aspects; [
+        window-manager.homeManager
+        desktop.homeManager
+        mac.homeManager
+        ssh.homeManager
+        llm.homeManager
+      ]);
   };
 
-  flake.homeConfigurations = lib.genAttrs
-    [
-      "x86_64-linux"
-      "aarch64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ]
-    (system: inputs.home-manager.lib.homeManagerConfiguration {
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
-      modules = baseModules;
-    });
+  flake.homeConfigurations =
+    lib.genAttrs
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ]
+      (
+        system:
+        inputs.home-manager.lib.homeManagerConfiguration {
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+          modules = baseModules;
+        }
+      );
 }
