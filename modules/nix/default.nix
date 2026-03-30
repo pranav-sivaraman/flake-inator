4d159a3417726c35ab38389d328544a3470e7189
Pranav Sivaraman <pranavsivaraman@gmail.com>
2026-03-29 21:33:47 -0400

    nix: cleanup config and add automatic gc
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -1,20 +1,32 @@
 let
-  sharedNixpkgsConfig = {
+  sharedNixConfig = {
     nixpkgs.config.allowUnfree = true;
+    nix = {
+      enable = true;
+      channel.enable = false;
+      gc = {
+        automatic = true;
+        options = "--delete-older-than 30d";
+      };
+      optimise.automatic = true;
+      settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
   };
 in
 {
   flake.aspects.nix = {
-    darwin = sharedNixpkgsConfig;
-    nixos = sharedNixpkgsConfig // {
-      nix = {
-        channel.enable = false;
-        optimise.automatic = true;
-        settings.experimental-features = [
-          "nix-command"
-          "flakes"
-        ];
+    darwin = sharedNixConfig // {
+      nix.gc.interval = {
+        Weekday = 0;
+        Hour = 0;
+        Minute = 0;
       };
+    };
+    nixos = sharedNixConfig // {
+      nix.gc.dates = "weekly";
     };
     homeManager = { };
   };
