3d80455aeb8e9ccce5b2a6562e54f05e4aeafd03
Pranav Sivaraman <pranavsivaraman@gmail.com>
2026-04-04 12:09:30 -0400

    containers: use podman on linux
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/programs/container/default.nix
+++ modules/programs/container/default.nix
@@ -2,13 +2,18 @@
   flake.aspects.container = {
     homeManager =
       { lib, pkgs, ... }:
-      lib.mkIf pkgs.stdenv.isDarwin {
-        services.colima.enable = true;
-        home = {
-          packages = with pkgs; [
-            docker
-          ];
-        };
-      };
+      lib.mkMerge [
+        (lib.mkIf pkgs.stdenv.isDarwin {
+          services.colima.enable = true;
+          home = {
+            packages = with pkgs; [
+              docker
+            ];
+          };
+        })
+        (lib.mkIf pkgs.stdenv.isLinux {
+          services.podman.enable = true;
+        })
+      ];
   };
 }
