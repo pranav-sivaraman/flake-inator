965e4d360fe65ed9e3c7a05541bda829a52761bf
Pranav Sivaraman <pranavsivaraman@gmail.com>
2026-06-14 13:11:47 -0400

    home-manager: cleanup module
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,4 +1,4 @@
-{ inputs, lib, ... }:
+{ inputs, ... }:
 let
   baseModules = with inputs.self.aspects; [
     psivaram.homeManager
@@ -6,44 +6,20 @@ let
     shell.homeManager
   ];
 
-  systems = [
-    "x86_64-linux"
-    "aarch64-linux"
-    "aarch64-darwin"
-  ];
-
-  mkHomeConfigurations =
-    name: modules:
-    lib.genAttrs (map (system: "${name}@${system}") systems) (
-      key:
-      let
-        system = lib.removePrefix "${name}@" key;
-        isDarwin = lib.hasSuffix "-darwin" system;
-        homeDir = if isDarwin then "/Users/${name}" else "/home/${name}";
-      in
-      inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = inputs.nixpkgs.legacyPackages.${system};
-        modules = modules ++ [
-          { home.homeDirectory = homeDir; }
-        ];
-      }
-    );
+  homeManagerConfig = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+  };
 in
 {
   flake.aspects.home-manager = {
     nixos = {
       imports = [ inputs.home-manager.nixosModules.home-manager ];
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-      };
+      home-manager = homeManagerConfig;
     };
     darwin = {
       imports = [ inputs.home-manager.darwinModules.home-manager ];
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-      };
+      home-manager = homeManagerConfig;
     };
   };
 
@@ -58,29 +34,4 @@ in
       ]);
   };
 
-  flake.homeConfigurations =
-    mkHomeConfigurations "psivaram" baseModules
-    // lib.genAttrs (map (system: "sivaramp@${system}") systems) (
-      key:
-      let
-        system = lib.removePrefix "sivaramp@" key;
-        isDarwin = lib.hasSuffix "-darwin" system;
-        homeDir = if isDarwin then "/Users/sivaramp" else "/home/sivaramp";
-      in
-      inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = inputs.nixpkgs.legacyPackages.${system};
-        modules = baseModules ++ [
-          (
-            { ... }:
-            {
-              home = {
-                username = lib.mkOverride 10 "sivaramp";
-                homeDirectory = lib.mkOverride 10 homeDir;
-              };
-              programs.bash.enable = lib.mkForce false;
-            }
-          )
-        ];
-      }
-    );
 }
