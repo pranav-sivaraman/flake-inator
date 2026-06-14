55b35af4a2ef1eda2dc0503ba98c0683e6447339
Pranav Sivaraman <pranavsivaraman@gmail.com>
2026-06-14 11:10:21 -0400

    ssh: simplify askpass
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/programs/ssh/default.nix
+++ modules/programs/ssh/default.nix
@@ -1,51 +1,24 @@
 {
   flake.aspects.ssh = {
     homeManager =
-      { pkgs, ... }:
+      { lib, pkgs, ... }:
       let
         skLib = "sk-libfido2${pkgs.stdenv.hostPlatform.extensions.sharedLibrary}";
 
-        # macOS-native SSH askpass based on https://github.com/theseal/ssh-askpass
-        ssh-askpass-mac = pkgs.writeScriptBin "ssh-askpass" ''
-          #!${pkgs.stdenv.shell}
+        ssh-askpass-mac = pkgs.writeShellScriptBin "ssh-askpass" ''
           exec /usr/bin/osascript - "$@" <<'APPLESCRIPT'
           on run argv
-              set args to argv as text
-              considering numeric strings
-                  set pre_catalina to (system version of (system info)) < "10.15"
-              end considering
-              if pre_catalina then
-                  set agent to POSIX file "/Applications/Utilities/Terminal.app/Contents/Resources/Terminal.icns"
-              else
-                  set agent to POSIX file "/System/Applications/Utilities/Terminal.app/Contents/Resources/Terminal.icns"
-              end if
-              set dialog_timeout to 15
+            set message to argv as text
+            set prompt to system attribute "SSH_ASKPASS_PROMPT"
 
-              set prompt to system attribute "SSH_ASKPASS_PROMPT"
-              # prompt = "confirm" is the same as our else, so just let it pass though
-              if prompt is "none" then
-                  display dialog args with icon agent default button 1 buttons { "OK" }
-              else if args ends with ": " or args ends with ":" then
-                  if args contains "pass" or args contains "pin" then
-                      display dialog args with icon agent default button 2 default answer "" with hidden answer
-                  else
-                      display dialog args with icon agent default button 2 default answer ""
-                  end if
-                  return result's text returned
-              else if args contains " host " then
-                  display dialog args with icon agent default button 1 cancel button 2 buttons {"OK", "No", "Yes"} default answer ""
-                  set host_key_result to result
-                  if text returned of host_key_result is not "" then
-                      return text returned of host_key_result
-                  else
-                      return button returned of host_key_result
-                  end if
-              else
-                  display dialog args with icon agent default button 1 giving up after dialog_timeout
-                  if gave up of result then
-                      error result
-                  end if
-              end if
+            if prompt is "none" then
+              display dialog message buttons {"OK"} default button "OK" with title "SSH authentication"
+            else if prompt is "confirm" then
+              display dialog message buttons {"No", "Yes"} default button "Yes" cancel button "No" with title "SSH authentication"
+            else
+              display dialog message default answer "" with hidden answer with title "SSH authentication"
+              return text returned of result
+            end if
           end run
           APPLESCRIPT
         '';
@@ -64,49 +37,50 @@
           }
         );
       in
-      {
-        home.packages = with pkgs'; [
-          openssh
-          ssh-askpass-mac
-        ];
+      lib.mkMerge [
+        {
+          home.packages = [ pkgs'.openssh ] ++ lib.optionals pkgs.stdenv.isDarwin [ ssh-askpass-mac ];
 
-        programs.ssh = {
-          enable = true;
-          enableDefaultConfig = false;
-          settings = {
-            "github.com" = {
-              User = "git";
-            };
-            "*" = {
-              Compression = true;
-              ControlMaster = "auto";
-              ControlPath = "/tmp/%r@%h:%p";
-              ControlPersist = "yes";
-              ForwardAgent = true;
-              # ForwardX11 = true;
-              # ForwardX11Trusted = true;
-              ServerAliveCountMax = 15;
-              ServerAliveInterval = 15;
-              SetEnv = {
-                TERM = "xterm-256color";
+          programs.ssh = {
+            enable = true;
+            enableDefaultConfig = false;
+            settings = {
+              "github.com" = {
+                User = "git";
               };
-              SecurityKeyProvider = "${pkgs'.openssh}/lib/${skLib}";
+              "*" = {
+                Compression = true;
+                ControlMaster = "auto";
+                ControlPath = "/tmp/%r@%h:%p";
+                ControlPersist = "yes";
+                ForwardAgent = true;
+                # ForwardX11 = true;
+                # ForwardX11Trusted = true;
+                ServerAliveCountMax = 15;
+                ServerAliveInterval = 15;
+                SetEnv = {
+                  TERM = "xterm-256color";
+                };
+                SecurityKeyProvider = "${pkgs'.openssh}/lib/${skLib}";
+              };
             };
           };
-        };
 
-        services.ssh-agent = {
-          enable = true;
-          package = pkgs'.openssh;
-          pkcs11Whitelist = [ "${pkgs'.openssh}/lib/${skLib}" ];
-        };
+          services.ssh-agent = {
+            enable = true;
+            package = pkgs'.openssh;
+            pkcs11Whitelist = [ "${pkgs'.openssh}/lib/${skLib}" ];
+          };
 
-        launchd.agents.ssh-agent.config.EnvironmentVariables = {
-          SSH_SK_PROVIDER = "${pkgs'.openssh}/lib/${skLib}";
-          SSH_ASKPASS = "${ssh-askpass-mac}/bin/ssh-askpass";
-          SSH_ASKPASS_REQUIRE = "prefer";
-          DISPLAY = ":0";
-        };
-      };
+        }
+        (lib.mkIf pkgs.stdenv.isDarwin {
+          launchd.agents.ssh-agent.config.EnvironmentVariables = {
+            SSH_SK_PROVIDER = "${pkgs'.openssh}/lib/${skLib}";
+            SSH_ASKPASS = "${ssh-askpass-mac}/bin/ssh-askpass";
+            SSH_ASKPASS_REQUIRE = "prefer";
+            DISPLAY = ":0";
+          };
+        })
+      ];
   };
 }
