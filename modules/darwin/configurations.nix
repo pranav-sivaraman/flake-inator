af80dc38e3b4270c60dd3d4cc5822314d5c35430
Pranav Sivaraman <pranavsivaraman@gmail.com>
2026-06-14 11:30:50 -0400

    darwin: inline default.nix and just have configurations
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/darwin/configurations.nix
+++ modules/darwin/configurations.nix
@@ -1,46 +1,83 @@
 { inputs, ... }:
-let
-  darwinSystem =
-    modules:
-    inputs.nix-darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      modules =
-        with inputs.self.aspects;
-        [
-          nix.darwin
-          psivaram.darwin
-          darwin.base
-          darwin.homebrew
-          home-manager.darwin
-        ]
-        ++ modules;
-    };
-in
 {
   flake.darwinConfigurations = {
-    personal = darwinSystem [
-      {
-        homebrew.casks = [
-          "kobo"
-          "calibre"
-          "tailscale-app"
-          "steam"
-          "prismlauncher"
-        ];
-      }
-    ];
+    Pranavs-MacBook-Air = inputs.nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = with inputs.self.aspects; [
+        nix.darwin
+        psivaram.darwin
+        home-manager.darwin
+        {
+          system.stateVersion = 5;
+          nix.enable = false;
 
-    work = darwinSystem [
-      {
-        home-manager.users.psivaram = {
-          programs.ssh = {
-            includes = [ "~/.ssh/config.hosts" ];
-            matchBlocks."*" = {
-              user = "sivaramp";
+          system = {
+            defaults = {
+              screencapture.target = "clipboard";
+              NSGlobalDomain = {
+                # Need to logout to apply this
+                NSStatusItemSpacing = 10;
+                NSStatusItemSelectionPadding = 6;
+              };
+              menuExtraClock = {
+                ShowAMPM = true;
+                ShowDayOfWeek = true;
+                ShowDayOfMonth = true;
+                ShowDate = 1;
+              };
+              controlcenter = {
+                AirDrop = true;
+                Sound = true;
+                FocusModes = true;
+                Display = true;
+                NowPlaying = false;
+                Bluetooth = true;
+                BatteryShowPercentage = true;
+              };
+              dock = {
+                autohide = true;
+                mru-spaces = false;
+                show-recents = false;
+              };
+              finder = {
+                AppleShowAllExtensions = true;
+                ShowPathbar = true;
+                FXDefaultSearchScope = "SCcf";
+              };
+              trackpad = {
+                Clicking = true;
+                TrackpadCornerSecondaryClick = 2;
+              };
+            };
+            keyboard = {
+              enableKeyMapping = true;
+              remapCapsLockToControl = true;
             };
           };
-        };
-      }
-    ];
+
+          homebrew = {
+            enable = true;
+            casks = [
+              "flux-app"
+              "yubico-authenticator"
+              "google-chrome"
+              "kobo"
+              "calibre"
+              "tailscale-app"
+              "steam"
+              "prismlauncher"
+            ];
+            onActivation = {
+              cleanup = "uninstall";
+              autoUpdate = true;
+              upgrade = true;
+              extraFlags = [
+                "--force-cleanup"
+              ];
+            };
+          };
+        }
+      ];
+    };
   };
 }
