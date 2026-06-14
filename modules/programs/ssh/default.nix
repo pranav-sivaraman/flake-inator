3509344ab68916a6e75430023da505ea1fcc2224
Pranav Sivaraman <pranavsivaraman@gmail.com>
2026-06-14 11:28:45 -0400

    ssh: merge aspect files
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/programs/ssh/default.nix
+++ modules/programs/ssh/default.nix
@@ -1,5 +1,19 @@
 {
   flake.aspects.ssh = {
+    nixos = {
+      services = {
+        openssh = {
+          enable = true;
+          settings = {
+            PasswordAuthentication = false;
+            KbdInteractiveAuthentication = false;
+            PermitRootLogin = "no";
+            AllowUsers = [ "psivaram" ];
+          };
+        };
+      };
+    };
+
     homeManager =
       { lib, pkgs, ... }:
       let
