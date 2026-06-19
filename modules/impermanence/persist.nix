746f14e0efec5b15b1df3114b6d9c57fe7c61ed8
Pranav Sivaraman <pranavsivaraman@gmail.com>
2026-06-19 15:23:28 -0400

    systemd: remove recursive directory workarounds
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/impermanence/persist.nix
+++ modules/impermanence/persist.nix
@@ -1,56 +1,33 @@
 { inputs, ... }:
 {
   flake.aspects.impermanence = {
-    nixos =
-      { lib, config, ... }:
-      {
-        imports = [ inputs.impermanence.nixosModules.impermanence ];
-        environment.persistence."/persist" = {
-          enable = true;
-          hideMounts = true;
-          directories = [
-            "/var/lib/nixos"
-            "/var/log/systemd"
-            "/var/log/journal"
-            "/var/log/lastlog"
-            {
-              # TODO: make this a virtual or something
-              directory = "/var/lib/postgresql";
-              user = "postgres";
-              group = "postgres";
-              mode = "0750";
-            }
-          ];
-          files = [
-            "/etc/machine-id"
-            "/etc/ssh/ssh_host_ed25519_key"
-            "/etc/ssh/ssh_host_ed25519_key.pub"
-            "/etc/ssh/ssh_host_rsa_key"
-            "/etc/ssh/ssh_host_rsa_key.pub"
-            "/etc/zfs/zpool.cache"
-          ];
-        };
-
-        # Recursively fix ownership of any persisted directory that has a non-root owner.
-        # Impermanence only chowns the top-level dir, not its contents, so subdirs can
-        # accumulate wrong ownership after UID reshuffling. 'Z' = recursive relabel.
-        systemd.tmpfiles.rules = lib.concatMap (
-          dir: lib.optional (dir.user != "root") "Z ${dir.dirPath} - ${dir.user} ${dir.group} - -"
-        ) (lib.concatMap (p: p.directories) (lib.attrValues config.environment.persistence));
-
-        # Ensure users/groups exist before tmpfiles applies recursive ownership (Z rules)
-        # for persisted directories.
-        systemd.services = lib.mkIf config.services.userborn.enable {
-          systemd-tmpfiles-setup = {
-            after = [ "userborn.service" ];
-            wants = [ "userborn.service" ];
-          };
-
-          systemd-tmpfiles-resetup = {
-            after = [ "userborn.service" ];
-            wants = [ "userborn.service" ];
-          };
-        };
+    nixos = _: {
+      imports = [ inputs.impermanence.nixosModules.impermanence ];
+      environment.persistence."/persist" = {
+        enable = true;
+        hideMounts = true;
+        directories = [
+          "/var/lib/nixos"
+          "/var/log/systemd"
+          "/var/log/journal"
+          "/var/log/lastlog"
+          {
+            # TODO: make this a virtual or something
+            directory = "/var/lib/postgresql";
+            user = "postgres";
+            group = "postgres";
+            mode = "0750";
+          }
+        ];
+        files = [
+          "/etc/machine-id"
+          "/etc/ssh/ssh_host_ed25519_key"
+          "/etc/ssh/ssh_host_ed25519_key.pub"
+          "/etc/ssh/ssh_host_rsa_key"
+          "/etc/ssh/ssh_host_rsa_key.pub"
+          "/etc/zfs/zpool.cache"
+        ];
       };
+    };
   };
 }
