1b9a7773772202a28fa271f2246818ca972ef961
Pranav Sivaraman <pranavsivaraman@gmail.com>
2026-08-01 12:31:10 -0400

    reverse-proxy: update sha256
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/services/reverse-proxy/default.nix
+++ modules/services/reverse-proxy/default.nix
@@ -128,7 +128,7 @@
 
                   caddyWithCloudflare = pkgs.caddy.withPlugins {
                     plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-                    hash = "sha256-wHW0l15aLswe7gV9WioXo//abd0sJI82I7zIroRG3uU=";
+                    hash = "sha256-7g8zDx5RhbptXFyEPtexxkHX8hw/gF001bZ7wX4Mjhs=";
                   };
                 in
                 {
