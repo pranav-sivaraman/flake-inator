50faa0098368c7558e20ac112da796583bd85fc1
Pranav Sivaraman <pranavsivaraman@gmail.com>
2026-08-05 22:49:13 -0400

    nono: add sandboxing tool
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/programs/llm.nix
+++ modules/programs/llm.nix
@@ -1,7 +1,12 @@
 {
   flake.modules.homeManager.default = {
-    home.sessionVariables = {
-      PI_OFFLINE = "1";
+    home = {
+      sessionVariables = {
+        PI_OFFLINE = "1";
+      };
+      shellAliases = {
+        pi = "nono run --profile nolabs-ai/pi --allow-cwd -- pi";
+      };
     };
     programs = {
       pi-coding-agent = {
--- modules/programs/tools.nix
+++ modules/programs/tools.nix
@@ -14,6 +14,7 @@
         nix-output-monitor
         nvd
         texliveFull
+        nono
       ];
     };
 }
