0f3cddcab1c577ed1e24bdc57ddf370834eb8b7a
Pranav Sivaraman <pranavsivaraman@gmail.com>
2026-08-05 22:29:00 -0400

    pi: remove config stuff
    Signed-off-by: Pranav Sivaraman <pranavsivaraman@gmail.com>

--- modules/programs/llm.nix
+++ modules/programs/llm.nix
@@ -6,13 +6,6 @@
     programs = {
       pi-coding-agent = {
         enable = true;
-        settings = {
-          defaultProvider = "deepseek";
-          defaultModel = "deepseek-v4-flash";
-          packages = [
-            "git:github.com/ayghri/i-have-adhd"
-          ];
-        };
       };
     };
   };
